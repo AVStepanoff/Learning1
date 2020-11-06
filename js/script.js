6a9b3a6629c04ec18ccbeaa31701c7f921c60b5b
Aleksey <psstudio.info@gmail.com>
2020-11-07 00:57:07 +0300

    Добавление функций в работу программы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?","");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?","");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?","");
+    }
+}
+
+start();
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,17 +20,27 @@ let personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
 
-    let answer1 = prompt("Один из последних посмотренных фильмов?", ""),
-        answer2 = +prompt("На сколько оцените его?", "");
-
-    if (answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 50) {
-        personalMovieDB.movies[answer1] = answer2;
-    } else {
-        i--;
+        let answer1 = prompt("Один из последних посмотренных фильмов?", ""),
+            answer2 = +prompt("На сколько оцените его?", "");
+    
+        if (answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 50) {
+            personalMovieDB.movies[answer1] = answer2;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
+
+rememberMyFilms();
+
+// То же самое, но используем другие виды циклов:
+
+// работаем с циклом while
 
 // let i = 0;
 // while (i < 2) {
@@ -35,6 +56,8 @@ for (let i = 0; i < 2; i++) {
 //     i++;
 // }
 
+// работаем с циклом do - while
+
 // let i = 0;
 // do {
 //     let answer1 = prompt("Один из последних посмотренных фильмов?", ""),
@@ -49,15 +72,40 @@ for (let i = 0; i < 2; i++) {
 //     i++;
 // } while (i < 2);
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    alert('Вы киноман');
-} else {
-    alert('Ошибка');
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Ошибка');
+    }
 }
 
-    console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB() {
+    if(!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {     
+            
+        let answer = prompt(`Ваш любимый жанр под номером ${i}`,'');  
+        if(answer != null || answer != '') {
+            personalMovieDB.genres[i-1] = answer;
+        } else {
+            i--;
+        }
+                   
+    }
+}
+
+writeYourGenres();
 
